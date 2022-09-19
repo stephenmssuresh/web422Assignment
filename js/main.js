564cfc3a3d2160f7d51af5161ae7a4cc0a89c303
stephenmsuresh <stephenmssuresh@gmail.com>
2022-09-19 13:35:21 -0400

    remove unnecessary files

--- js/main.js
+++ js/main.js
@@ -1,25 +1,54 @@
 var page = 1;
 var perPage = 10;
+var movieArr = []; //empty array to store all the movies from the fetch
 
 var loadMovieData = function (title = null) {
-  //   var element = document.getElementById("pageNum");
-  //   element.classList.add("d-none");
+  var element = document.getElementById("pageNum");
+  element.classList.add("d-none");
   // https://stackoverflow.com/questions/44217376/fetch-api-not-working-with-localhost-127-0-0-1
   // https://github.github.io/fetch/
   // https://stackoverflow.com/questions/35038857/setting-query-string-using-fetch-get-request
-  fetch(`http://localhost:8080/api/movies?page=1&perPage=10`)
-    .then((response) => {
-      console.log(response.json());
-      return response.json();
-    })
-    .then((json) => {
-      console.log(json); // here is the parsed JSON response
-    });
+  if (!title) {
+    // fetch query params
+    // https://stackoverflow.com/questions/35038857/setting-query-string-using-fetch-get-request
+    fetch(
+      "https://busy-cyan-drill-veil.cyclic.app/api/movies?" +
+        new URLSearchParams({
+          'page': page,
+          'perPage': perPage
+        })
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        movieArr = data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  else{
+    fetch(
+      "https://busy-cyan-drill-veil.cyclic.app/api/movies?" +
+        new URLSearchParams({
+          'page': page,
+          'perPage': perPage,
+          'title': title
+        })
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        movieArr = data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 };
 
-loadMovieData();
-
-fetch('https://busy-cyan-drill-veil.cyclic.app/api/movies?page=1&perPage=10')
-.then(function(response) {
-  console.log(response.json());
-});
+var trElements = function() {
+  
+}
