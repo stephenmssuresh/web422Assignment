b2a84beee5319c31287d56b77e603280f6d659c6
stephenmsuresh <stephenmssuresh@gmail.com>
2022-09-23 10:36:30 -0400

    shorten loadData using $ and {} and  wrote minutesToTime()

--- js/main.js
+++ js/main.js
@@ -2,53 +2,45 @@ var page = 1;
 var perPage = 10;
 var movieArr = []; //empty array to store all the movies from the fetch
 
-var loadMovieData = function (title = null) {
+function loadData(title = null) {
   var element = document.getElementById("pageNum");
   element.classList.add("d-none");
-  // https://stackoverflow.com/questions/44217376/fetch-api-not-working-with-localhost-127-0-0-1
-  // https://github.github.io/fetch/
-  // https://stackoverflow.com/questions/35038857/setting-query-string-using-fetch-get-request
-  if (!title) {
-    // fetch query params
-    // https://stackoverflow.com/questions/35038857/setting-query-string-using-fetch-get-request
-    fetch(
-      "https://busy-cyan-drill-veil.cyclic.app/api/movies?" +
-        new URLSearchParams({
-          'page': page,
-          'perPage': perPage
-        })
-    )
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        movieArr = data;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-  else{
-    fetch(
-      "https://busy-cyan-drill-veil.cyclic.app/api/movies?" +
-        new URLSearchParams({
-          'page': page,
-          'perPage': perPage,
-          'title': title
-        })
-    )
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        movieArr = data;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
+  let url = title
+    ? `https://busy-cyan-drill-veil.cyclic.app/api/movies?page=${page}&perPage=${perPage}&title=${title}`
+    : `https://busy-cyan-drill-veil.cyclic.app/api/movies?page=${page}&perPage=${perPage}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      movieArr = data;
+      console.log(movieArr);
+    })
+    .catch((err) => console.log(err));
+}
+
+function createRows() {
+  let movieList = `${movieArr.map(
+    (movies) =>
+      `<tr data-id=${movies._id}>
+      <td>${movies.year}</td>
+      <td>${movies.title}</td>
+      <td>${movies.plot}</td>
+      <td>${movies.rated}</td>
+      <td>${minutesToTime(movies.runtime)}</td>
+    </tr>`
+  )}`;
+
+  console.log(movieList);
+}
+var trElements = function () {};
+
+
+ function minutesToTime (time) {
+  let t = parseInt(time);
+  let hours = Math.trunc((t / 60));
+  let minutes = t % 60;
+  let hm = hours.toString().concat(":", minutes.toString());
+  return hm;
 };
 
-var trElements = function() {
-  
-}
+minutesToTime(136);
